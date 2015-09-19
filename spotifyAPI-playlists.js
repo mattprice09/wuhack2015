f9fe3377f41d847f5e47708f29f36b5ecbcdc53d
Vinesh Kannan <vingkan@gmail.com>
2015-09-19 16:40:25 -0500

    Trying to get playlists

--- spotifyAPI-playlists.js
+++ spotifyAPI-playlists.js
@@ -15,3 +15,21 @@ function createPlaylist(eventTitle){
 		}
 	});
 }
+
+function getPlaylist(ownerID, playlistID){
+	$.ajax({
+		type: "POST",
+		url: spotifyBase + 'v1/users/' + ownerID + '/playlists/' + playlistID + '?market=ES',
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("Authorization", "Bearer " + bearer)
+			
+		},
+		success: function(data){
+			console.log(data);
+		}
+	});
+}
+
+var owner = "1285594060";
+var playlist = "2r7tkFG3aLznuVaLTyaIcb";
+getPlaylist(owner, playlist);
--- spotifyAPI.js
+++ spotifyAPI.js
@@ -1,7 +1,7 @@
 var clientID = "afa826690fe04e05a65c5b92ec2bec26";
 var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
-var bearer = "BQCYBsOuRWpzccwfVYIYIH5Ijq8IGl8Bfb4yKdEaNzKnx2U1TNfrXiZBKvTiQIZIIZmEyymgP0SMz_Hu24mL4n2aEHSHNyKYVSpMMMdtk7ahm";
+var bearer = "BQD_9Tc-zWyWKIWDhJxCKWQ7UOKFq0f__G1SXkQNNBODj_i9TrQCCED5X3feZvEhFDva330dIQrTw8i3cDLINepN4sTqQYImCJfq8kY9UTruxE65eHJCaZ-V3eId41B7XVYz7R4jExx__90uM8Y2NjMuDYUlz2lEaNlzX128FPU16TFy2MJw9Y93wEkEzJgbcr1v7tay0shXCGjSICLA1wLFp-ovXB1uTjbnHpp1_5dhfiSxR0zPpQ_atFifR-7IwWSR9mAcYXoGBZehEAYnQDqnXHGPQE3MWuEVG35omlg";
 
 function serveData(data){
 	var incomingData = document.getElementById('incomingData');
