3e4120c591f46f60e2caa3e2a771fc831aa26b1a
Vinesh Kannan <vingkan@gmail.com>
2015-09-19 09:00:33 -0500

    Created search structure with callbacks

--- songSort/spotifyAPI-search.js
+++ songSort/spotifyAPI-search.js
@@ -1,4 +1,13 @@
+function chooseFromResults(data){
+	console.log(data)
+	console.log(data.tracks)
+	console.log(data.tracks.items[0])
+	return data.tracks[0];
+}
+
 function getSearchData(data){
+	var bestResult = chooseFromResults(data);
+	console.log(bestResult);
 	return {
 		'id': 0
 	};
@@ -10,7 +19,7 @@ function handleSearchData(searchData){
 
 function getSearch(query, type){
 	$.ajax({
-		url: spotifyBase + 'v1/search?q=/' + query + '&type=' + type,
+		url: spotifyBase + 'v1/search?q=' + query + '&type=' + type,
 		beforeSend: function(xhr) {
 			xhr.setRequestHeader("Authorization", "Bearer " + bearer)
 		},
@@ -22,3 +31,5 @@ function getSearch(query, type){
 		}
 	});
 }
+
+getSearch('uptown+funk', 'track')
