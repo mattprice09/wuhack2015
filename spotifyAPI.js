fb52b9dce5bac8a1485708eda7c5dba2361c3ab8
Vinesh Kannan <vingkan@gmail.com>
2015-09-19 11:46:21 -0500

    Added White Square Logo + New OAuth Token

--- spotifyAPI.js
+++ spotifyAPI.js
@@ -1,7 +1,7 @@
 var clientID = "afa826690fe04e05a65c5b92ec2bec26";
 var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
-var bearer = "BQA6DbvKxFGbs-fqVpFJWvOR0CSZwm38hvRHi8ossEkj4X5qGxxn2JPwNte-ZEYXSsLatnFCCWNGx9OapWtfERWz3aA4ulCLxg9ey-2JgZqUwfNCmX1uPWsxrJ5HSiqOnTTU83zgqNwih8v_qTgUssMUmv7-oqDkP3GuwBBRSllZe-1HeoulqM2QU2vOI5ZUC9dakKhy2wHgwfkYZ5wLY1MQEFisp8LKWrE-wgNuPYy4gEqgQlqsqacfBbrK51hQ1j9Ab_xHMjrYgxp7rO6FhDdpRBovlkVJYGfAzY-3pL4";
+var bearer = "BQB32FQ52LZDULfnPTGPwAQSsJaGbNc4cZW95zpixicDCkWJK-w9t7hC8npz603EgkpEJepvhq_ETESdciYVNcb2ilk5wPNP5-BgQGd5tsIWAH-rpPOa-HV-cjV_bG13DwGj8FT-qPkJh3Hm_THihYxq7D-23t1d7aqTY8C7mD26ciOYd3UXE7cVNyEpFKAvO2d9BPmLvQ5QRVeFCFxbh9mh6vZkexvDLGY7HHgLAUg3E066KrGxVb4eRIVFSksnBl0Q5HAqH9F1bTF3zk3Qh0inEQy1ezwfYx-Yn6pZHfc";
 
 function serveData(data){
 	var incomingData = document.getElementById('incomingData');
