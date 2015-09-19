ebf1d4ad3f32060fd8e36e9e9e2f612dd5d4326a
Vinesh Kannan <vingkan@gmail.com>
2015-09-19 02:14:46 -0500

    Added iframe Spotify player
    Takes track id on call and changes player

--- songSort/spotifyAPI.js
+++ songSort/spotifyAPI.js
@@ -2,11 +2,17 @@ var clientID = "afa826690fe04e05a65c5b92ec2bec26";
 var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
 var bearer = "BQC65ap1YKqT7yUSOGWZZ0_drwd1DzLtX2dhPTKSjMp5qhvnIJds-fzCbKV4RNnQB86BlAxsBiZrSFPK_1U96qiSpcMyCdGXoW4kW6D9ZArEcJLjd0BarL4sTlP_eB0A2tosIKcp5VkiVVJB77fexNTV21eCFqW_XnDbw6qaQtCOQXUixpbe32gwixFP-e8_yzVqqlMlnopN9teLDMkPvZYZsR1BJU9iOPk8NbLqtFsiNJE4fougiVVV3BvvQogOTiyUWGXyJthxYynGG3dmz_mcvMKnxYR-nbbl587uBVo";
-var incomingData = document.getElementById('incomingData');
 
 function serveData(data){
+	var incomingData = document.getElementById('incomingData');
 	var jsonData = JSON.stringify(data);
 	incomingData.value += jsonData + "\n";
+}
+
+function sendToPlayer(trackID){
+	var player = document.getElementById('player');
+	var iframe = '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A' + trackID + '" width="100%" height="200" frameborder="0" allowtransparency="true"></iframe>';
+	player.innerHTML = iframe;
 }
 
 function getTrackData(data){
@@ -19,7 +25,7 @@ function getTrackData(data){
 }
 
 function handleTrackData(trackData){
-	
+	sendToPlayer(trackData.id);
 }
 
 function getTrack(trackID){
