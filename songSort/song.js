3f9de253b56ae23d03f2eb7c2df059a16697ddb5
Vinesh Kannan <vingkan@gmail.com>
2015-09-18 23:26:14 -0500

    Added getters and setters

--- songSort/song.js
+++ songSort/song.js
@@ -1,11 +1,66 @@
 function Song(title, artist, genre, userID){
+	// Input  Variables
 	this.title = title;
 	this.artist = artist;
 	this.genre = genre;
 	this.users = [];
-	this.users.push(userID);
+	this.users.push(userID || 'anonUser');
+	this.score = 0;
+}
+
+Song.prototype.calculateScore = function(){
+
 }
 
 Song.prototype.toString = function(){
 	return '[' + this.users.length + '] ' + this.title + ' by ' + this.artist + ' (' + this.genre + ')';
+}
+
+/*--------------------------------------------*/
+/*---> GETTERS & SETTERS <--------------------*/
+/*--------------------------------------------*/
+
+Song.prototype.getTitle = function(){
+	return this.title;
+}
+
+Song.prototype.setTitle = function(title){
+	this.title = title;
+}
+
+Song.prototype.getArtist = function(){
+	return this.artist;
+}
+
+Song.prototype.setArtist = function(artist){
+	this.artist = artist;
+}
+
+Song.prototype.getGenre = function(){
+	return this.genre;
+}
+
+Song.prototype.setGenre = function(){
+	this.genre = genre;
+}
+
+Song.prototype.getUsers = function(){
+	return this.users;
+}
+
+/*
+* Note that userID is an array of userIDs, be sure to wrap in [] brackets, even when pushing only a single userID
+*/
+Song.prototype.addUser = function(userID){
+	for(var u = 0; u < userID.length; u++){
+		this.users.push(userID[u]);
+	}
+}
+
+Song.prototype.getScore = function(){
+	return this.score;
+}
+
+Song.prototype.setScore = function(score){
+	this.score = score;
 }
