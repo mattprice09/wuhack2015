d6e55594930e7c2560fe51794ffad952bcff5798
Vinesh Kannan <vingkan@gmail.com>
2015-09-19 15:31:41 -0500

    Send JSON Data to Database

--- application.js
+++ application.js
@@ -1,3 +1,12 @@
+function getSongJSON(trackID){
+	for(var s = 0; s < jukebox.getSongs().length; s++){
+		if(jukebox.getSongs()[s].getID() == trackID){
+			return JSON.stringify(jukebox.getSongs()[s]);
+		}
+	}
+}
+
 function postTrack(trackID){
-	alert("POST: " + trackID + " TO SERVER.");
+	var json = getSongJSON(trackID);
+	alert("POST: " + json + " TO SERVER.");
 }
