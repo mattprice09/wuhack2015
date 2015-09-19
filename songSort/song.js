106d379cf376fca1879e4a67c7d9963eb9a53e31
Vinesh Kannan <vingkan@gmail.com>
2015-09-18 23:10:31 -0500

    Added song.toString()

--- songSort/song.js
+++ songSort/song.js
@@ -5,3 +5,6 @@ function Song(title, artist, genre){
 	this.users = 1;
 }
 
+Song.prototype.toString = function(){
+	return '[' + this.users + '] ' + this.title + ' by ' + this.artist + ' (' + this.genre + ')';
+}
