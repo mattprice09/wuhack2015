c19bda4f903eaf02157e9a55f8be6227819e1f23
Yongzheng Huang <iamhuangyz@gmail.com>
2015-09-19 21:40:49 -0500

    implemented add delete update methods for firebase DB

--- application.js
+++ application.js
@@ -1,15 +1,38 @@
 function getSongJSON(trackID){
-	for(var s = 0; s < jukebox.getSongs().length; s++){
-		if(jukebox.getSongs()[s].getID() == trackID){
-			return JSON.stringify(jukebox.getSongs()[s]);
-		}
+    for(var s = 0; s < jukebox.getSongs().length; s++){
+	if(jukebox.getSongs()[s].getID() == trackID){
+	    return JSON.stringify(jukebox.getSongs()[s]);
 	}
+    }
 }
 
 function postTrack(trackID){
-	//alert("POST: " + trackID + " TO SERVER.");
-	var track = getTrack(trackID);
-	addTrackToDB();
-	var json = getSongJSON(trackID);
-	alert("POST: " + json + " TO SERVER.");
+    //alert("POST: " + trackID + " TO SERVER.");
+    var track = getTrack(trackID);
+    addTrackToDB();
+    var json = getSongJSON(trackID);
+    alert("POST: " + json + " TO SERVER.");
 }
+
+// get a reference of the database
+var ref = new Firebase("https://amber-torch-7758.firebaseio.com");
+
+// add song method
+var addSong = function(songJSON) {
+    songRef = ref.child(songJSON.id);
+    songRef.set(songJSON);
+};
+
+// delete a song from the list
+var deleteSong = function(songID) { 
+    ref.child(songID).remove();
+};
+
+var update = function(songID, key, value) {
+    ref.child(songID).child(key).set(value);
+};
+
+// read all objects from the database at once
+var readAll = function(callback) {
+    ref.once("value", callback);
+};
