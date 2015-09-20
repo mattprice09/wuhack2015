1068ae5ffa07081d0fe315813fbbd01659e19325
Yongzheng Huang <iamhuangyz@gmail.com>
2015-09-20 05:57:17 -0500

    added id

--- application.js
+++ application.js
@@ -3,19 +3,36 @@
 var ref = new Firebase("https://amber-torch-7758.firebaseio.com");
 var songsRef = new Firebase("https://amber-torch-7758.firebaseio.com/songs");
 
+// init
+var initID = function() {
+    ref.update({"id": 1});
+};
+
+var increamentID = function() {
+    ref.child("id").once("value", function(data) {
+	console.log(data.val());
+	ref.update({"id": data.val() + 1});
+    });
+};
+
+initID();
+increamentID();
+increamentID();
+increamentID();
+
 // add song method
 var addSong = function(songJSON) {
-    songRef = ref.child("songs").child(songJSON.id);
+    songRef = ref.child("jukebox").child(songJSON.id);
     songRef.set(songJSON);
 };
 
 // delete a song from the list
 var deleteSong = function(songID) { 
-    ref.child("songs").child(songID).remove();
+    ref.child("jukebox").child(songID).remove();
 };
 
 var update = function(songID, key, value) {
-    ref.child("songs").child(songID).child(key).set(value);
+    ref.child("jukebox").child(songID).child(key).set(value);
 };
 
 // read all objects from the database and also continuously read when a Song is added
@@ -25,11 +42,12 @@ var readAll = function(callback) {
     });
 };
 
-/*var updateAll = function(callback) {
-    ref.on("value", function(snapshot)) {
-        Console.log(snapshot);
-    }
-};*/
+// add jukebox
+var addJukebox = function() {
+
+};
+
+
 
 // Update all when value is changed
 ref.on("value", function(snapshot) {
@@ -41,7 +59,7 @@ ref.on("value", function(snapshot) {
 	    songs.push(dataToSong(snapshotObj[key]));
 	}
     }
-    Console.log(songs[0] + "......this is the one");
+    console.log(songs[0]);
 
     jukebox.addSongs(songs);
 
