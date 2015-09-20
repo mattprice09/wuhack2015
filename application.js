c0305dbbfcfee113c7e41fc52943eae0ff35dd68
Yongzheng Huang <iamhuangyz@gmail.com>
2015-09-19 23:30:24 -0500

    add songs folder in DB

--- application.js
+++ application.js
@@ -4,17 +4,17 @@ var ref = new Firebase("https://amber-torch-7758.firebaseio.com");
 
 // add song method
 var addSong = function(songJSON) {
-    songRef = ref.child(songJSON.id);
+    songRef = ref.child("songs").child(songJSON.id);
     songRef.set(songJSON);
 };
 
 // delete a song from the list
 var deleteSong = function(songID) { 
-    ref.child(songID).remove();
+    ref.child("songs").child(songID).remove();
 };
 
 var update = function(songID, key, value) {
-    ref.child(songID).child(key).set(value);
+    ref.child("songs").child(songID).child(key).set(value);
 };
 
 // read all objects from the database at once
@@ -22,6 +22,7 @@ var readAll = function(callback) {
     ref.once("value", callback);
 };
 
+/*
 var cSong = {
     "artist": "ColdPlay",
     "artistID": "7dGJo4oG8kP0tJRR",
@@ -31,6 +32,7 @@ var cSong = {
     "score": 1,
     "title": "Yellowwwwww"
 };
+*/
 
 function getSongJSON(trackID){
     for(var s = 0; s < jukebox.getSongs().length; s++){
