b9a971e2fd8f042ad30e9f45ad1af18e76f97c02
mattprice09 <mattprice09@rocketmail.com>
2015-09-20 06:35:54 -0500

    gjg

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 /* DB API */
 // get a reference of the database
 var ref = new Firebase("https://amber-torch-7758.firebaseio.com");
-var songsRef = new Firebase("https://amber-torch-7758.firebaseio.com/songs");
+var songsRef = new Firebase("https://amber-torch-7758.firebaseio.com/jukebox");
 
 var incrementID = function() {
     ref.child("id").once("value", function(data) {
@@ -27,11 +27,15 @@ var update = function(songID, key, value) {
 // read all objects from the database and also continuously read when a Song is added
 var readAll = function(callback) {
     songsRef.on("child_added", function(snapshot) {
-        song = datatoSong(snapshot.val());
+        song = dataToSong(snapshot.val());
         song.isQuery = false;
         jukebox.addSongs([song]);
     });
 };
+
+var getJukeboxID = function(JBname) {
+
+}
 
 // add jukebox
 var addJukebox = function() {
@@ -70,3 +74,24 @@ function postTrack(trackID){
     json.isQuery = false;
     addSong(json);
 }
+
+/**function toHex(number) {
+    hex = "";
+    num = number;
+    while (num > 0) {
+        hex = hex + (num % 16);
+        num = (num/16);
+    }
+    console.log(hex.length);
+    hexGood = "";
+    for (i = 0; i < hex.length; i++) {
+        if (hex.length < 2) {
+            hexGood = hex;
+        } else {
+            hexGood = hexGood + hex.charAt(hex.length - (i + 1));
+    }
+    }
+    return hexGood;
+}
+
+console.log(toHex(16));*/
