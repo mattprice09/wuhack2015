2a017a3ec9d9b4852aadd58d74a39a3e496942f0
Vinesh Kannan <vingkan@gmail.com>
2015-09-20 07:08:15 -0500

    Refreshed OAuth Token

--- spotifyAPI.js
+++ spotifyAPI.js
@@ -2,7 +2,7 @@ var clientID = "afa826690fe04e05a65c5b92ec2bec26";
 var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
 
-var bearer = "BQD2OYkUF5PDhNqFIMYHQyExbQo-A3LZfWFe6LZigcL8IFpVYc4-qJBeNTTFhtC0v6ZFoPBPka-nVwuHgj1BS7sTV8FyLnGyjGt8WF6ZGIGRlUBlFjzIn3XSsEDVuzN0dPAOgZxUZV1tfCT4dwygyL6ZRLeyEbVzka9N6SEnq1ptkuDeUHn3pwnYiQvcNOetoB_jsa7dgkUzg38Bgym_MoxJsP-ulXWOfgfsEYngvSwBKMq11N5MUp-5Bwuy4hJrpHbjzkC0Yh-xwkkDOBa-ou6vwkN7-tr-rYOK3ZIYnNs";
+var bearer = "BQDtDFZkM7bv0JzsgDN4UPwhgwEFdIYNbmHBdEIvKzTwSTkKME9juoatfcOhdis0_g35KXFc1eQbwt4EvCyLubKsQUyOFTCJsqb9fsPd540HcopLTNEqvOlJgeVT3rXQTPGxWdhO9PuXCcYw-R3uvfnBmaILafPOZa_eYB1-Cqz9ysQVOuPDsPUXg5vK-qkvUt5mKn07KmCeJWy_k4xCiYw6PQWcViPZmCAjykLjSuJHBuqMNQVgqB5Hary6q5aslDKV1_fhdzbxn0YpvOCIGy1hBbvd2YHCYIJxlCAIhOU";
 
 function serveData(data){
 	var incomingData = document.getElementById('incomingData');
