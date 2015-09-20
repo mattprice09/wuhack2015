29164c5d8de7919a54b53279907f6fac0f62872d
Vinesh Kannan <vingkan@gmail.com>
2015-09-20 06:06:09 -0500

    Refreshed Key

--- spotifyAPI.js
+++ spotifyAPI.js
@@ -2,7 +2,7 @@ var clientID = "afa826690fe04e05a65c5b92ec2bec26";
 var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
 
-var bearer = "BQCSiLEnnqyuPRP-1_z2AX9AumxkdiclA-_zYuLC2oYJ3WTI5Dl_D3oKpTRZ6TpccDx-N7iq7fu5c9u2_hPDQQA2p4xd9P17SDd_6RafVEZxhmcqLUuAxOFyezYpIHeGLne6cNmCfixFlb9x1lQbD2t99-UXBR0RlZ5hMPj9K1sqdfaMbHBkauqIrdEZNMHeZAaOps1yMtS_inujYJo7b5aqhY4DPjxp_pwcjOTwgN_w-Yjx1KvL-3v-wWsMLC1uzAD_SI9SsMEPWPlVMg1ii86QJHB0hBf8bitjpR2VD2U";
+var bearer = "BQD2OYkUF5PDhNqFIMYHQyExbQo-A3LZfWFe6LZigcL8IFpVYc4-qJBeNTTFhtC0v6ZFoPBPka-nVwuHgj1BS7sTV8FyLnGyjGt8WF6ZGIGRlUBlFjzIn3XSsEDVuzN0dPAOgZxUZV1tfCT4dwygyL6ZRLeyEbVzka9N6SEnq1ptkuDeUHn3pwnYiQvcNOetoB_jsa7dgkUzg38Bgym_MoxJsP-ulXWOfgfsEYngvSwBKMq11N5MUp-5Bwuy4hJrpHbjzkC0Yh-xwkkDOBa-ou6vwkN7-tr-rYOK3ZIYnNs";
 
 function serveData(data){
 	var incomingData = document.getElementById('incomingData');
