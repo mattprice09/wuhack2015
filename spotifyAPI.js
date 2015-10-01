43bbf30383e7956fed3d6b92430f5013f3d60e2c
Yongzheng Huang <iamhuangyz@gmail.com>
2015-09-30 23:57:41 -0500

    token changed

--- spotifyAPI.js
+++ spotifyAPI.js
@@ -3,7 +3,7 @@ var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
 
 
-var bearer = "BQCVDf5ge6aCMQ6nOUFiTMNMe-jQMA7mJYy1d8hZRZCSdDxNf61MdykWafAmTIPADfFDUbiMTp-HEesJ7ge3M2RPAPmD2taQ6eISxaBFcXGbUpzgvLGdmBhS0RbcvAM3bXJPi8EHiFnJYof8GHSYbjpR0TukV3xEgBwV9gFA_vwPsypd1wjO8aO1lVd1HYN-E8TjgT99YUdLxUwUVIZNbaYAcF7N7ihe30PSeSP0EG_X3y-1Gtz86NrAvs-Lmd2BLnNPvKR3WdKvH_ZMnNkFkxqJQgFp5jUtpbAf7dQ6iRc";
+var bearer = "BQDNWf3lvAnPARVEOmr7yPt3vlqyiwwckTPJekRiWDHC5apE4mKn5O8i7pY16cmrgxm_8FsOOR_bbAounRipzfeGs5MMuceSWDl1bmIoVowkOW8SPfjInvggnG-zDIljNuElC-tgurmQ7oaTs12n5G3FyrkrTj-pt1weu8Ihva5nUYL23vHOn5GhCJ3Ixgsro4UN75NRcHCM_zP4rz28ZM0FBltkOXuKxZaeatC8XEwf4E9hK4-PIg0cGp7TWv_yThkBhE8Xwu0kJRKziippcwoAJBR_2QhrjLVfxQ";
 
 /*
 * Temporary backdoor method of updating OAuth key
